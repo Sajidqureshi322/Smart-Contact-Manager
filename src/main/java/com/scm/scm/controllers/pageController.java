4eb8c3f28891bce3c737d27c0073c8548e645022
SajidQureshi <qureshisajid322@gmail.com>
2024-09-12 16:41:10 +0530

    Validation done

--- src/main/java/com/scm/scm/controllers/pageController.java
+++ src/main/java/com/scm/scm/controllers/pageController.java
@@ -6,9 +6,11 @@ import com.scm.scm.helpers.Message;
 import com.scm.scm.helpers.MessageType;
 import com.scm.scm.services.UserService;
 import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -74,14 +76,16 @@ public class pageController {
     //processing register
 
     @RequestMapping(value = "/do-register", method = RequestMethod.POST)
-    public String processRegister(@ModelAttribute UserForm userForm, HttpSession session) {
+    public String processRegister(@Valid @ModelAttribute UserForm userForm, BindingResult rBindingResult, HttpSession session) {
         System.out.println("Processing registration");
         // fetch form data
         // UserForm
         System.out.println(userForm);
 
         // validate form data
-        // TODO::Validate userForm[Next Video]
+        if(rBindingResult.hasErrors()){
+            return "register";
+        }
 
         // save to database
 
--- src/main/java/com/scm/scm/forms/UserForm.java
+++ src/main/java/com/scm/scm/forms/UserForm.java
@@ -1,6 +1,9 @@
 package com.scm.scm.forms;
 
-import lombok.AllArgsConstructor;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import  lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,10 +18,18 @@ import lombok.ToString;
 @ToString
 public class UserForm {
 
+    @NotBlank(message = "Username is required")
+    @Size(min = 3,message = "Min 3 characters are required")
     private String name;
+    @Email(message = "Invalid Email Address")
+    @NotBlank(message = "Email is required")
     private String email;
+    @NotBlank(message = "Password is required")
+    @Size(min = 6,message = "Min 6 characters are required")
     private String password;
+    @NotBlank(message = "About is required")
     private String about;
+    @Size(min = 8,max = 12,message = "Invalid Phone Number")
     private String phoneNumber;
 
 }
